fd802df12013ae0ee2a78e4cbc3c5b9b90a304b3
R3MO DAS <remodas7774@gmail.com>
2023-07-30 19:09:07 +0530

    changes made to the code

--- script.js
+++ script.js
@@ -1,82 +1,75 @@
-const bg = document.getElementById("bg");
-const bgcolor = document.querySelector(".bg-overlay");
+const bottle = document.querySelector(".bg__overlay");
+const greenBg = document.querySelector(".bg__layer");
 const heading = document.querySelector(".heading");
-const rotateimg = document.querySelector(".rotatingimg");
-const textPop = document.querySelector(".text-popup");
-const firstSect = document.querySelector("#first-section");
-const firstsectCont = document.querySelector(".firstsectCont");
+const rotatingSvg = document.querySelector(".rotatingimg");
+const textBox = document.querySelector(".text__box");
+
+const secondSect = document.querySelector("#second__section");
+const secondSectContent = document.querySelector(".second__content");
+
+const logo = document.querySelector(".logo__2");
+const fade = document.querySelector(".fade__effect");
+
 const box = document.querySelector(".box");
-const logo = document.querySelector(".logoimg2");
-const fade = document.querySelector(".fade-effect-nav");
 
 window.addEventListener("scroll", () => {
-    let value = window.scrollY;
-    bg.style.backgroundSize = 250 + value * 5.3 + "px";
-    bgcolor.style.opacity = 1 - value / 500;
-    heading.style.opacity = 1 - value / 500;
-    rotateimg.style.opacity = 1 - value / 500;
+  const scrollValue = window.scrollY;
 
-    if (value <= 500) {
-        textPop.style.scale = 0 + value / 500;
-        textPop.style.opacity = 0 + value / 500;
-    }
+  bottle.style.backgroundSize = 250 + scrollValue * 5.3 + "px";
+  heading.style.opacity = 1 - scrollValue / 500;
+  rotatingSvg.style.opacity = 1 - scrollValue / 500;
+  greenBg.style.opacity = 1 - scrollValue / 500;
 
-    if (value == 500) {
-        textPop.style.scale = "1";
-    }
+  if (scrollValue <= 500) {
+    textBox.style.scale = 0 + scrollValue / 500;
+    textBox.style.opacity = 0 + scrollValue / 500;
+  }
 
-    if (value == 600) {
-        textPop.style.scale = "1.04";
-    }
+  if (scrollValue == 500) {
+    textBox.style.scale = "1";
+  }
 
-    if (value == 700) {
-        textPop.style.scale = "1.05";
-    }
+  if (scrollValue == 600) {
+    textBox.style.scale = "1.04";
+  }
 
-    if (value == 800) {
-        textPop.style.scale = "1.06";
-    }
+  if (scrollValue == 700) {
+    textBox.style.scale = "1.05";
+  }
 
-    if (value == 900) {
-        textPop.style.scale = "1.08";
-    }
+  if (scrollValue == 800) {
+    textBox.style.scale = "1.06";
+  }
 
-    if (value < 1200) {
-        firstSect.style.position = "static";
-        firstsectCont.style.display = "none";
-    }
+  if (scrollValue == 900) {
+    textBox.style.scale = "1.08";
+  }
 
-    if (value >= 1200) {
-        firstSect.style.position = "relative";
-        firstSect.style.top = "2000px";
-        firstsectCont.style.display = "block";
-    }
+  if (scrollValue < 1200) {
+    secondSect.style.position = "static";
+    secondSectContent.style.display = "none";
+  }
 
-    if(value < 2100){
-        logo.style.opacity = 0;
-        fade.style.display = "none";
-    }
+  if (scrollValue >= 1200) {
+    secondSect.style.position = "relative";
+    secondSect.style.top = "2000px";
+    secondSectContent.style.display = "block";
+  }
 
-    if(value >= 2100){
-        logo.style.transition = "all 0.2s linear";
-        logo.style.opacity = 1;
-        logo.style.position = "fixed";
-        logo.style.top = "0";
-        fade.style.display = "block";
-    }
+  if (scrollValue < 2100) {
+    logo.style.opacity = 0;
+    fade.style.display = "none";
+  }
 
-    if (value >= 3300) {
-        box.style.scale = 0.8 + (value - 2000) / 4000;
-    }
-})
+  if (scrollValue >= 2100) {
+    logo.style.transition = "all 0.2s linear";
+    logo.style.opacity = 1;
+    logo.style.position = "fixed";
+    logo.style.top = "0";
+    fade.style.display = "block";
+  }
 
-const menu = document.querySelector(".ham__menu");
-const close = document.querySelector(".close__menu");
-const navbar = document.querySelector(".header__ul");
-menu.addEventListener('click', ()=>{
-    navbar.classList.add("active");
-})  
-
-close.addEventListener('click',()=>{
-    navbar.classList.remove("active");
-})
+  if (scrollValue >= 3300) {
+    box.style.scale = 0.8 + (scrollValue - 2000) / 4000;
+  }
+});
